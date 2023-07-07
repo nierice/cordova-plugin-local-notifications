20c8f9c34eb8ce196d649d779f1b612f75a806a5
Nieric Javinal <nieric.g.javinal@sunlife.com>
2023-07-07 17:41:11 +0800

    Fix Android 31 SDK click trigger

--- src/android/notification/receiver/AbstractClickReceiver.java
+++ src/android/notification/receiver/AbstractClickReceiver.java
@@ -28,6 +28,8 @@ import android.os.Bundle;
 import de.appplant.cordova.plugin.notification.Manager;
 import de.appplant.cordova.plugin.notification.Notification;
 
+import static android.content.Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
+import static android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP;
 import static de.appplant.cordova.plugin.notification.action.Action.CLICK_ACTION_ID;
 import static de.appplant.cordova.plugin.notification.action.Action.EXTRA_ID;
 
