9521318dab275364585200a267c1915c5091a041
Nieric Javinal <nieric.g.javinal@sunlife.com>
2023-07-07 18:02:17 +0800

    Remove override

--- src/android/notification/receiver/AbstractClickReceiver.java
+++ src/android/notification/receiver/AbstractClickReceiver.java
@@ -48,7 +48,6 @@ abstract public class AbstractClickReceiver extends NotificationTrampolineActivi
     /**
      * Called when local notification was clicked to launch the main intent.
      */
-    @Override
     protected void onHandleIntent(Intent intent) {
         // this.intent        = intent;
 
